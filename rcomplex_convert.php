9aaca1ebead55d29a4e3da801386bf6be1598555
森崇 <kanetugu2015@gmail.com>
2022-12-02 18:39:40 +0900

    bug fixed

--- rcomplex_convert.php
+++ rcomplex_convert.php
@@ -17,9 +17,9 @@ $json_array = load_json($map_json);
 
 $src_obj_name = key($json_array["src"]);
 #printf("src_obj_name=%s\n", $src_obj_name);
-printf("INFO: src_obj_fpath=%s\n", $json_array["src"][$src_obj_name]);
+printf("INFO: src_obj_fpath=%s\n", $json_array["src"][$src_obj_name]["filepath"]);
 
-$src_csv_obj = new CsvFileIo($json_array["src"][$src_obj_name]);
+$src_csv_obj = new CsvFileIo($json_array["src"][$src_obj_name]["filepath"]);
 $dst_objs = array();
 foreach ($json_array["dsts"] as $obj) {
     $dst_obj_name = key($obj);
@@ -32,8 +32,8 @@ foreach ($json_array["dsts"] as $obj) {
         $obj[$dst_obj_name]["pkeys"]);
 }
 
-$relation = new CsvRelation($json_array["dst_relations"]);
-$relation->get_value(1, $dst_objs, "out1.out2.out3", "email");
+$relation = new CsvRelation($json_array["dst_relations"], $dst_objs);
+$relation->set_value(1, "out1.out2.tell", "tmori");
 #$start_line_src = (int)$json_array["start_line_src"];
 #$start_line_dst = (int)$json_array["start_line_dst"];
 #$src_pkeys = $src_csv_obj->get_colinx_array($json_array["src_pkeys"]);
--- utils/CsvRelation.php
+++ utils/CsvRelation.php
@@ -4,45 +4,67 @@ Class CsvRelation
 {
     private $relation_definitions;
     private $map_ids = array();
-    function __construct($relation_definitions)
+    private $objs;
+    function __construct($relation_definitions, $objs)
     {
+        $this->objs = $objs;
         $this->relation_definition = $relation_definitions;
         foreach ($relation_definitions as $relation) {
             $key = $relation["parent"] . "." . $relation["child"];
             $value = [
-                "p"=> $relation["parent_colname"],
-                "c"=> $relation["child_colname"]
+                "p"=> $relation["parent_colnames"]
             ];
             $this->map_ids[$key] = $value;
         }
     }
-
-    function get_value($parent_row, $objs, $path, $name)
+    private function row($parent_row, $path)
     {
         $path_array = explode('.', $path);
         $parent = $path_array[0];
         $num = count($path_array);
-        $last_inx = $num - 1;
-        for ($i = 1; $i < $num; $i++) {
+        $name = $path_array[$num - 1];
+        $last_inx = $num - 2;
+        for ($i = 1; $i < $num - 1; $i++) {
             $parent = $path_array[$i - 1];
             $child = $path_array[$i];
-            #printf("parent=%s\n", $parent);
             $key = $parent . "." . $child;
-            #printf("tmp key=%s\n", $this->map_ids[$key]["p"]);
-            $colinx = $objs[$parent]->colinx($this->map_ids[$key]["p"]);
-            $parent_value = $objs[$parent]->get_pkeys($parent_row, [ $colinx ]);
-            #printf("parent_value=%s\n", $parent_value);
-            #printf("child=%s\n", $child);
-            $colinx = $objs[$child]->colinx($this->map_ids[$key]["c"]);
-            #TODO
-            $child_row = $objs[$child]->get_value_by_pkey($objs[$child]->start_line(), [ $colinx ], $parent_value);
+            $columns = $this->objs[$parent]->get_colinx_array($this->map_ids[$key]["p"]);
+            $parent_value = $this->objs[$parent]->get_pkeys($parent_row, $columns);
+            $child_row = $this->objs[$child]->get_value_by_pkey_with_cache($parent_value);
             if ($i == $last_inx) {
-                $child_value = $objs[$child]->value($child_row, $objs[$child]->colinx($name));
-                printf("%s.%s=%s\n", $path, $name, $child_value);
+                return $child_row;
             }
         }
+        return null;
+    }
+
+    public function value($parent_row, $path)
+    {
+        $tmp = explode('.', $path);
+        $target = $tmp[count($tmp) - 2];
+        $name = $tmp[count($tmp) - 1];
+        $row = $this->row($parent_row, $path);
+        if ($row) {
+            $value = $this->objs[$target]->value($row, $this->objs[$target]->colinx($name));
+            printf("%s.%s=%s\n", $path, $name, $value);
+            return $value;
+        }
         return NULL;
-    }    
+    }
+    public function set_value($parent_row, $path, $value)
+    {
+        $tmp = explode('.', $path);
+        $target = $tmp[count($tmp) - 2];
+        $name = $tmp[count($tmp) - 1];
+        $row = $this->row($parent_row, $path);
+        if ($row) {
+            $this->objs[$target]->set_value($row, $this->objs[$target]->colinx($name), $value);
+            printf("%s.%s=%s\n", $path, $name, $value);
+            return $value;
+        }
+        return NULL;
+    }
+
 }
 
 ?>
